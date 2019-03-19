2022e3df9b8ec74c5e7e04912e5c6e80ef5b0952
Ernie Val <erndval@gmail.com>
2019-03-19 20:18:47 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,14 +1,14 @@
 dictionary = {
-  "hello" => "hi"
-  "for" => "4"
-  "four" => "4"
-  "to" => "2"
-  "two" => "2"
-  "too" => "2"
-  "be" => "b"
-  "you" => "u"
-  "at" => "@"
-  "and" => "&"
+  "hello" => "hi",
+  "for" => "4",
+  "four" => "4",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&",
 }
 
 def word_subsituter(tweet)
