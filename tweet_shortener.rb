1f7aa51fd10c0c45e055172856c3bfa178f9ef57
Ernie Val <erndval@gmail.com>
2019-03-19 20:42:52 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,9 +17,9 @@ def word_substituter(tweet)
   tweet_string.each do |word|
     dictionary.each do |entry, replacement|
       if word == entry
-        replacement << [tweet_sentence]
+        replacement << tweet_sentence
       else
-        word << [tweet_sentence]
+        word << tweet_sentence
       end
     end
   end
