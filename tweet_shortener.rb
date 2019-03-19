9a31b0c6823d4a3047eddaea7d17cb6f0493bd55
Ernie Val <erndval@gmail.com>
2019-03-19 20:12:10 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,16 @@
 dictionary = {
+  "hello" => "hi"
+  "for" => "4"
+  "four" => "4"
+  "to" => "2"
+  "two" => "2"
+  "too" => "2"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"
 }
+
+def word_subsituter(tweet)
+  
   
